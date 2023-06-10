476a05c4853c4e5b04170c60c3e88a55dfb8b18c
Atticus Young <atticuscyoung@icloud.com>
2023-06-09 20:10:51 -0500

    Modified main

--- src/main/kotlin/sorting.kt
+++ src/main/kotlin/sorting.kt
@@ -1,23 +1,1 @@
-var monsterList = mutableListOf (
-    Monster("Maui",2.0, 10, 20, 15),
-    Monster("Thomas",1.0, 10, 20, 15)
-
-)
-
-
-fun sortingByCombatRating() {
-    val copyOfPartyLevel = partyLevel
-    monsterList.sortByDescending{it.combatRating}
-    val currentEnemyParty = mutableListOf<Monster>()
-    while(copyOfPartyLevel > 0) {
-        for(i in monsterList.indices) {
-            if(copyOfPartyLevel > monsterList[i].combatRating)
-            {
-                copyOfPartyLevel - monsterList[i].combatRating
-                currentEnemyParty.add(monsterList[i])
-            }
-
-        }
-    }
-    print(currentEnemyParty)
-}
+val monsterList = mutableListOf<Monster>(    Monster("Maui",2.0, 10, 20, 15),    Monster("Thomas",1.0, 10, 20, 15))fun sortingByCombatRating() {    val copyOfPartyLevel = partyLevel    monsterList.sortByDescending{it.combatRating}    val currentEnemyParty = mutableListOf<Monster>()    while(copyOfPartyLevel > 0) {        for(i in monsterList.indices) {            if(copyOfPartyLevel > monsterList[i].combatRating)            {                copyOfPartyLevel - monsterList[i].combatRating                currentEnemyParty.add(monsterList[i])            }        }    }    print(currentEnemyParty)}
